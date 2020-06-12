5775c729c4cb6005c96e4611d35273d619dd54e7
jinghongluo <jil040@ucsd.edu>
2020-06-12 14:14:29 -0700

    First Push

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  // return <div>Jinghong Luo!</div>
+  // return <div style={{ color: `purple`, fontSize: `72px` }}>Hello Gatsby!</div> 
+  return(
+    <div style={{color: 'purple'}}>
+       <Link to="/contact/">Contact</Link>
+      <Header headerText="Jinghong Luo" />
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt=""/>
+    </div>
+
+  );
 }
